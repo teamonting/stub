6c83a18cd7007b28bc94114eba06bc36804af085
William Wong <compulim@hotmail.com>
2026-06-21 03:39:03 -0700

    Fix ESLint

--- packages/stub/src/implementation.ts
+++ packages/stub/src/implementation.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./types/selenium-webdriver/bidi/input.d.ts" />
 
 import type { StubImplementation } from '@onting/rpc';
